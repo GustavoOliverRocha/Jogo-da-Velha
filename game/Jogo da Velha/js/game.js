04ebd492a21036d6ebafb42a5f9d6c90ca91dece
Gustavo Oliveira Rocha <gustaoliver18@gmail.com>
2021-04-01 01:15:35 -0300

    V6
    -Criação da função checarVitoria() onde tira a necessidade de 2 fors pra cada jogador
    -Implementação da checagem de dar Velha

--- game/Jogo da Velha/js/game.js
+++ game/Jogo da Velha/js/game.js
@@ -4,9 +4,9 @@
 					 [0,0,0]];
 	var vez = "X";
 	var i;
-	var diago = 2;//diagonal
+	var jogadas=0;
 
-	function fimJogo(jogador,posicao)
+	function fimJogo(jogador)
 	{
 		switch(jogador)
 		{
@@ -16,10 +16,14 @@
 			case 2:
 				$("#vez").css({"background-color":"#87CEFA","border-radius":"5px","width":"40%"});
 			break
-			default: alert("Deu velha");
+			default: 
+				$("#vez").css({"background-color":"#7B68EE"})
+				$(".row0").css({"background-color":"#7B68EE"})
+				$(".row1").css({"background-color":"#7B68EE"})
+				$(".row2").css({"background-color":"#7B68EE"});
 		}
 
-		document.getElementById("vez").innerHTML = "Fim de Jogo!!!<br>Vencedor: Jogador "+ jogador+"<br>Posição: " + posicao;
+		document.getElementById("vez").innerHTML = "Fim de Jogo!!!<br>Vencedor: Jogador "+ jogador;
 
 		$(".row0").removeAttr("onclick");
 		$(".row1").removeAttr("onclick");
@@ -38,13 +42,14 @@
 			alert("Essa parte ja foi escolhida");
 			return;
 		}
+
 		// vez do jogador 1 'X'
 		else if (vez == "X") 
 		{
 			//Colocando o X onde foi clicado
 			setJogada(row,col,"#DC143C","&#10006;");
 
-			//Marcando a posição do X na matriz
+			//Marcando a posição do 1('X') na matriz
 			tabuleiro[row][col] = 1;
 
 			//Passando a vez para o proximo
@@ -61,12 +66,39 @@
 			document.getElementById("vez").innerHTML = "Agora é a vez do: " + vez;
 		}
 
-		//Fim do Jogo
+		if(checarVitoria(1,"#B22222"))
+			fimJogo(1);
+		else if(checarVitoria(2,"#87CEEB"))
+			fimJogo(2);
+		else
+			jogadas++;
+
+		//Caso todas as Jogadas foram feitas vai dar velha
+		if(jogadas == 9)
+		{
+			//Tendo certeza de que nenhum dos dois jogadores venceram
+			if(!checarVitoria(1,"#B22222") && !checarVitoria(2,"#B22222"))
+				fimJogo("Velha");
+		}		
+	}
+
+	//Colocando a jogada X ou O no tabuleiro e estilizando elas
+	function setJogada(linha,col,cor,obj)
+	{
+		document.getElementById("linha" + linha + "_col" + col).innerHTML = obj;
+		$("#linha" + linha + "_col" + col).css({"color":cor,"text-shadow": "3px 2px 3px black"});
+	}
+
+	//Fim do Jogo?
+	function checarVitoria(jogador,cor)
+	{
+		//diagonal
+		var diago = 2;
 		for(i = 0;i < 3;i++)
 		{
-			//Condições de vencer jogador 1
+			//Condições de vencer jogadores 1 e 2 
 			//Caso ele vença na Vertical | | |
-			if(tabuleiro[0][i] == 1 && tabuleiro[1][i] == 1 && tabuleiro[2][i] == 1)
+			if(tabuleiro[0][i] == jogador && tabuleiro[1][i] == jogador && tabuleiro[2][i] == jogador)
 			{
 				/*Por questão de visual e localização esse i2 servirá para marcar aonde que o jogador venceu
 				* no caso de ser na Vertical a linha(row) terá um valor variado entre 0,1 e 2
@@ -75,94 +107,43 @@
 				*/
 				for(var i2 = 0;i2 < 3;i2++)
 				{
-					$("#linha" + i2 + "_col" + i).css({"background-color":"#00FF7F"});
+					$("#linha" + i2 + "_col" + i).css({"background-color":cor});
 				}
-				fimJogo(1,"vertical");
-				return;
+				//fimJogo(jogador,"vertical");
+				return true;
 			}
 
 			//Caso ele vença na Horizontal - - -
-			else if(tabuleiro[i][0] == 1 && tabuleiro[i][1] == 1 && tabuleiro[i][2] == 1)
+			else if(tabuleiro[i][0] == jogador && tabuleiro[i][1] == jogador && tabuleiro[i][2] == jogador)
 			{
 				for(var i2 = 0;i2 < 3;i2++)
 				{
-					$("#linha" + i + "_col" + i2).css({"background-color":"#00FF7F"});
+					$("#linha" + i + "_col" + i2).css({"background-color":cor});
 				}
-				fimJogo(1,"Horizontal");
-				return;
+				//fimJogo(jogador,"Horizontal");
+				return true;
 			}
 
 			//Caso ele vença na Diagonal (ambas direita e esquerda)
-			else if(tabuleiro[0][0] == 1 && tabuleiro[1][1] == 1 && tabuleiro[2][2] == 1 ||
-					tabuleiro[i][i+2] == 1 && tabuleiro[i+1][i+1] == 1 && tabuleiro[i+2][i] == 1)
+			else if(tabuleiro[0][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][2] == jogador ||
+					tabuleiro[i][i+2] == jogador && tabuleiro[i+1][i+1] == jogador && tabuleiro[i+2][i] == jogador)
 			{
 				
 				for(var i2 = 0;i2 < 3;i2++)
 				{
-					if (tabuleiro[i][i+2] == 1 && tabuleiro[i+1][i+1] == 1 && tabuleiro[i+2][i] == 1)
+					if (tabuleiro[i][i+2] == jogador && tabuleiro[i+1][i+1] == jogador && tabuleiro[i+2][i] == jogador)
 					{
-						$("#linha" + i2 + "_col" + diago--).css({"background-color":"#00FF7F"});
+						$("#linha" + i2 + "_col" + diago--).css({"background-color":cor});
 					}
 					else
 					{
-						$("#linha" + i2 + "_col" + i2).css({"background-color":"#00FF7F"});
+						$("#linha" + i2 + "_col" + i2).css({"background-color":cor});
 					}
 				}
-				fimJogo(1,"Diagonal");
-				return;
+				//fimJogo(jogador,"Diagonal");
+				return true;
 			}
-
-
-			//Condições de vencer jogador 2
-			//Caso ele vença na Vertical | | |
-			else if(tabuleiro[0][i] == 2 && tabuleiro[1][i] == 2 && tabuleiro[2][i] == 2)
-			{
-				for(var i2 = 0;i2 < 3;i2++)
-				{
-					$("#linha" + i2 + "_col" + i).css({"background-color":"#00FF7F"});
-				}
-				fimJogo(2,"vertical");
-				return;
-			}
-
-			//Caso ele vença na Horizontal - - -
-			else if(tabuleiro[i][0] == 2 && tabuleiro[i][1] == 2 && tabuleiro[i][2] == 2)
-			{
-				for(var i2 = 0;i2 < 3;i2++)
-				{
-
-					$("#linha" + i + "_col" + i2).css({"background-color":"#00FF7F"});
-				}
-				fimJogo(2,"Horizontal");
-				return;
-			}
-
-			//Caso ele vença na Diagonal (ambas direita e esquerda)
-			else if(tabuleiro[0][0] == 2 && tabuleiro[1][1] == 2 && tabuleiro[2][2] == 2 ||
-					tabuleiro[i][i+2] == 2 && tabuleiro[i+1][i+1] == 2 && tabuleiro[i+2][i] == 2)
-			{
-				for(var i2 = 0;i2 < 3;i2++)
-				{
-					if (tabuleiro[i][i+2] == 2 && tabuleiro[i+1][i+1] == 2 && tabuleiro[i+2][i] == 2)
-					{
-						$("#linha" + i2 + "_col" + diago--).css({"background-color":"#00FF7F"});
-					}
-					else
-					{
-						$("#linha" + i2 + "_col" + i2).css({"background-color":"#00FF7F"});
-					}
-				}
-				fimJogo(2,"Diagonal");
-				return;
-			}
-
 		}
-
+		//Se o jogo não detectar nenhum jogada vitoriosa retorna falso
+		return false;
 	}
-	function setJogada(linha,col,cor,obj)
-	{
-		document.getElementById("linha" + linha + "_col" + col).innerHTML = obj;
-		$("#linha" + linha + "_col" + col).css({"color":cor,"text-shadow": "3px 2px 3px black"});
-	}
-
-
